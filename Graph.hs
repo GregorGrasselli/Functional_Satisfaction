9ddc7bd0e9d80dcc0eb21f2fc703c2ab67b5f2f7
Gregor Grasselli <gregor.grasselli@gmail.com>
2015-01-08 00:13:06 +0100

    dodal primer

--- Graph.hs
+++ Graph.hs
@@ -7,14 +7,14 @@ import BasicEnumeration
 -- | A graph is given as a list of edges. Vertex indices start with 0!
 type Graph = [(Int, Int)]
 
-condA g k bvar = L.foldl' f bTrue $ take (size g) [0..]
-    where f a e = bAnd a $ L.foldl' bOr bFalse (map bvar (map ((show e) ++) $ map show [0..k]))
+condA g k bvar = foldr f bTrue $ take (size g) [0..]
+    where f e a = bAnd a $ foldr bOr bFalse (map bvar (map ((show e) ++) $ map show [0..k]))
 
-condB g k bvar = L.foldl' f bTrue $ take (size g) [0..]
-    where f a e = bAnd a $ L.foldl' bAnd bTrue ( map (\(x, y) -> bNot $ bAnd (bvar x) (bvar y)) [(show e ++ show b1, show e ++ show b2) | b1 <-  [0..k], b2 <- [0..k], b1 /= b2])
+condB g k bvar = foldr f bTrue $ take (size g) [0..]
+    where f e a = bAnd a $ foldr bAnd bTrue ( map (\(x, y) -> bNot $ bAnd (bvar x) (bvar y)) [(show e ++ show b1, show e ++ show b2) | b1 <-  [0..k], b2 <- [0..k], b1 /= b2])
 
-condC g k bvar = L.foldl' f bTrue g
-    where f a (j, t) = bAnd a $ bNot $ L.foldl' bOr bFalse (map (\b -> bAnd (bvar $ show j ++ show b) (bvar $ show t ++ show b)) [0..k])
+condC g k bvar = foldr f bTrue g
+    where f (j, t) a = bAnd a $ bNot $ foldr bOr bFalse (map (\b -> bAnd (bvar $ show j ++ show b) (bvar $ show t ++ show b)) [0..k])
 
 wellColored g k bvar = condA g k bvar `bAnd` condB g k bvar `bAnd` condC g k bvar
 
--- Primeri.hs
+++ Primeri.hs
@@ -4,7 +4,7 @@ import Graph
 import MPlusEnumeration
 import BasicEnumeration
 import qualified Data.List as L
-import qualified Data.HashMap as H (empty, Map, filter)
+import qualified Data.HashMap as H (empty, Map, filter, fromList)
 import Control.Monad
 
 -- | Makes a graph that looks like a polygon with 2 d diagonals to its closest neighbors.
@@ -12,7 +12,7 @@ polygonalGraph :: Int -> Int -> Graph
 polygonalGraph s d = L.concatMap f [0..s-1]
     where f i = take (d + 1) $ zip (repeat i) (iterate ((`mod` s) . (+ 1)) ((i + 1) `mod` s))
 
-findGraphSolution :: (Monad m, MonadPlus m) => Int -> Int -> Int -> m (H.Map String Bool, Bool)
+findGraphSolution :: MonadPlus m => Int -> Int -> Int -> m (H.Map String Bool, Bool)
 findGraphSolution n d k = wellColored (polygonalGraph n d) k bVarl (\ e -> return (filterEnv e, True)) (\ _ -> mzero) H.empty
    where filterEnv = H.filter id
 
@@ -27,3 +27,7 @@ long = ((bNot (bVarl "x") `bOr` bVarl "y") `bOr` bVarl "z") `bOr` bVarl "z"
 
 short :: MonadPlus m => (Env Bool -> m a) -> (Env Bool -> m a) -> Env Bool -> m a
 short = (bVarl "x" `bOr` (bNot (bVarl "x") `bOr` bVarl "y")) `bOr` bVarl "z"
+
+exampleToEvaluate = H.fromList [("80",False),("81",False),("82",True),("83",False),("93",False),("92",False),("91",False),("90",True),("40",False),("41",True),("42",False),("43",False),("53",False),("52",True),("51",False),("50",False),("62",False),("63",False),("60",True),("61",False),("71",True),("70",False),("73",False),("72",False),("00",True),("01",False),("02",False),("03",False),("13",False),("12",False),("11",True),("10",False),("22",True),("23",False),("20",False),("21",False),("31",False),("30",True),("33",False),("32",False),("112",True),("113",False),("110",False),("111",False),("101",True),("100",False),("103",False),("102",False)]
+
+evalPGraph n d k env = wellColored (polygonalGraph n d) k bVar (\_ -> True) (\_ -> False) env
